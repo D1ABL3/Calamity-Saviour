a112f9ad900f638128396b359043b8d2c39e5e07
rohitverma1999hack <rohitvermaavcbbk2011@gmail.com>
2019-10-12 02:35:58 +0530

    All Done

---
+++ app/src/main/java/com/example/rohit/butoom_animation/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.rohit.butoom_animation;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button b;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        b=(Button)findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Animation animation = AnimationUtils.loadAnimation(MainActivity.this,R.anim.bounce);
+                b.startAnimation(animation);
+            }
+        });
+
+
+    }
+}
